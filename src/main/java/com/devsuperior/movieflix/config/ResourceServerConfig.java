add51de3c22fa1bb652cdf042e26f39db58a716b
Eduardo Rodrigues <oraeduardo.gmail.com>
2022-08-23 15:18:03 -0300

    project completed and ready to be released for evaluation

--- src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -43,7 +42,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
-		//.antMatchers(HttpMethod.GET, VISITOR_OR_MEMBER).permitAll()
 		.antMatchers(VISITOR_OR_MEMBER).hasAnyRole("VISITOR", "MEMBER")
 		.antMatchers(MEMBER).hasRole("MEMBER")
 		.anyRequest().authenticated();
